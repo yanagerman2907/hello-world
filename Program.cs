32809c9725d3491b4ea4c0b137d421ae428765b6
yanagerman2907 <48985537+yanagerman2907@users.noreply.github.com>
2019-03-27 12:56:57 +0200

    Add files via upload
    Added files from Transportation

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle tachka = new Vehicle();
+            tachka.engine = EngineType.Diesel;
+            tachka.mileage = 4000;
+            tachka.Print();
+
+            // 
+            Console.WriteLine();
+            Console.WriteLine();
+            //
+
+            Car beha = new Car();
+            beha.brand = Brand.BMW;
+            beha.engine = EngineType.Gas;
+            beha.mileage = 5000;
+            
+            beha.Print();
+            Console.Write("New speed: ");
+
+            beha.Accelerate(500);
+            Console.Write(beha.speed);
+
+            // 
+            Console.WriteLine();
+            Console.WriteLine();
+            //
+
+            Bus busik = new Bus();
+            busik.engine = EngineType.Gas;
+            busik.mileage = 8000;
+            busik.seats = 400;
+            busik.Print();
+
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Vehicle.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transportation
+{
+    public enum EngineType
+    {
+        Diesel,
+        Gas
+    };
+
+    public enum Brand
+    {
+        BMW, Renault, Mercedes, Audi, Ferrari
+    }
+
+    class Vehicle
+        //class try 
+    {
+        public Vehicle(){ }
+
+        protected EngineType _engine;
+        protected double _speed=150;
+        protected double _mileage;
+
+
+        public EngineType engine        //New Comment
+        {
+            get;
+            set; 
+        }
+
+        public double mileage           //DONE
+        {
+            get; set;
+        }
+        
+        public double speed             //DONE
+        {
+            get { return _speed; }
+            set
+            {
+                if (value <= 0)
+                {
+                    _speed = 0;
+                }
+                else if (value >= 300)
+                {
+                    _speed = 300;
+                }
+                else
+                {
+                    _speed = value;
+                }
+            }
+        }
+
+
+        public void Accelerate(double kmph)
+        {
+            speed += kmph;
+        }
+
+        public void Decelerate(double kmph)
+        {
+            speed -= kmph;
+        }
+
+        public virtual void Print()
+        {
+            
+            Console.WriteLine("Engine: " + engine);
+            Console.WriteLine("Speed: "+ speed +" kmph");
+            Console.WriteLine("Mileage: " + mileage + " km");
+
+        }
+    }
+
+    
+
+    class Car : Vehicle
+    {   public Brand brand
+        {
+            get; set;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("         Car");
+            Console.WriteLine("Brand: " + brand);
+            base.Print();
+            
+        }
+    }
+
+    class Bus : Vehicle
+    {
+        protected double _seats = 50;
+        public double seats
+        {
+            get { return _seats; }
+            set
+            {  if (value <= 12)
+                    _seats = 12;
+                else if (value >= 50)
+                    _seats = 50;
+                else
+                    _seats = value;
+            }
+
+
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("         Bus");
+            base.Print();
+            Console.Write("A number of seats: " + seats);
+        }
+
+
+    }
+}
